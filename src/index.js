76cdf2a55719b3502d7b13887cbf4aeac9e17e03
antoniab79 <antonia@webleviathan.com>
2024-05-24 23:14:02 +0100

    added extra changes

--- src/index.js
+++ src/index.js
@@ -4,16 +4,38 @@ function refreshWeather(response) {
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#condition");
   let humidityElement = document.querySelector("#humidity");
-  let windSpeedElementw = document.querySelector("#wind-speed");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/ph`;
-
   temperatureElement.innerHTML = Math.round(temperature);
 }
 
+function formatDate(date) {
+  let minutes = date.getMinutes;
+  let hours = date.getHours;
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}: ${minutes}`;
+}
 function searchCity(city) {
   let apiKey = "1e5e2f20350d3toa44fe9d489eb954a7";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
